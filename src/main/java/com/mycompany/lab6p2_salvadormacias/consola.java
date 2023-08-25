cb3a0a20d38b2286b3e7dcaeb4b3f822b9838b14
papilon10 <140213599+papilon10@users.noreply.github.com>
2023-08-25 13:32:15 -0600

    avance clases

--- src/main/java/com/mycompany/lab6p2_salvadormacias/consola.java
+++ src/main/java/com/mycompany/lab6p2_salvadormacias/consola.java
@@ -9,5 +9,48 @@ package com.mycompany.lab6p2_salvadormacias;
  * @author papilon10
  */
 public class consola {
+
+    int tamano;
+    int batería;
+    boolean estuche;
+
+    public consola() {
+    }
+
+    public consola(int tamano, int batería, boolean estuche) {
+        this.tamano = tamano;
+        this.batería = batería;
+        this.estuche = estuche;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public int getBatería() {
+        return batería;
+    }
+
+    public void setBatería(int batería) {
+        this.batería = batería;
+    }
+
+    public boolean isEstuche() {
+        return estuche;
+    }
+
+    public void setEstuche(boolean estuche) {
+        this.estuche = estuche;
+    }
+
+    @Override
+    public String toString() {
+        return "consola{" + "tamano=" + tamano + ", bater\u00eda=" + batería + ", estuche=" + estuche + '}';
+    }
+    
     
 }
