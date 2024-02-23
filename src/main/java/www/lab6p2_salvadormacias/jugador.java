e72ec0e00a3ee9d5f21b444c1a1d8e94d4724a67
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-02-23 13:39:58 -0600

    segundo commit

--- src/main/java/www/lab6p2_salvadormacias/jugador.java
+++ src/main/java/www/lab6p2_salvadormacias/jugador.java
@@ -9,5 +9,47 @@ package www.lab6p2_salvadormacias;
  * @author Apple
  */
 public class jugador {
-    
+
+    String nombre;
+    int edad;
+    String posicion;
+
+    public jugador() {
+    }
+
+    public jugador(String nombre, int edad, String posicion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.posicion = posicion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    @Override
+    public String toString() {
+        return "jugador{" + "nombre=" + nombre + ", edad=" + edad + ", posicion=" + posicion + '}';
+    }
+
 }
